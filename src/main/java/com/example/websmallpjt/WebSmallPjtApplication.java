13bb28d4a8cc6d3e63ef195d4836a98ac61dbe3c
염평강 <pyeom@bitekic.co.kr>
2022-07-27 14:01:03 +0900

    FirstCommit

---
+++ src/main/java/com/example/websmallpjt/WebSmallPjtApplication.java
@@ -0,0 +1,13 @@
+package com.example.websmallpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSmallPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSmallPjtApplication.class, args);
+    }
+
+}
